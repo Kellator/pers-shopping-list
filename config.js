78bc0e5861399768b9e1f46e65a0fa21fdba13d0
Kelly Miller <k.macleod21@gmail.com>
2016-11-23 00:59:19 +0000

    added production url to mLab

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://<dbuser>:<dbpassword>@ds050879.mlab.com:50879/pers-shopping-list' :
                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
 
